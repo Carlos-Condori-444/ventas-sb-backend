051f9c2f97eec7c0445ad500ae3eeec4d254e2dd
Carlos-Condori-444 <carlos.condori.cho@gmail.com>
2025-12-02 03:35:48 -0500

    v1

--- src/main/java/com/sva/cm/api_ventas/ApiVentasApplication.java
+++ src/main/java/com/sva/cm/api_ventas/ApiVentasApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiVentasApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hola");
+		System.out.println("hola v1");
 		SpringApplication.run(ApiVentasApplication.class, args);
 	}
 
